eb7abea7d7b08fc11c364eb9825fda25edefe22c
Youngnam Lee <lyn00227@naver.com>
2022-06-09 17:19:20 +0900

    first commit

---
+++ src/main/java/com/lyn/pcode/PcodeApplication.java
@@ -0,0 +1,13 @@
+package com.lyn.pcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyn/pcode/PcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyn.pcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
