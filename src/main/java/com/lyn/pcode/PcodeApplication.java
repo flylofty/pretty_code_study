ce9b9d4feca760a0de9ac3ed1e91ff5aa7a893e3
Youngnam Lee <lyn00227@naver.com>
2022-06-13 15:07:19 +0900

    add time stamp

--- src/main/java/com/lyn/pcode/PcodeApplication.java
+++ src/main/java/com/lyn/pcode/PcodeApplication.java
@@ -2,7 +2,9 @@ package com.lyn.pcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
 @SpringBootApplication
 public class PcodeApplication {
 
