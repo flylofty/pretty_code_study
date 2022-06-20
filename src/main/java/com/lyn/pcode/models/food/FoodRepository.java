eb40bc863a7ead8173f2b896a48803c51a5f3642
Youngnam Lee <lyn00227@naver.com>
2022-06-20 16:35:05 +0900

    feat: 특정 음식점 메뉴 보기

--- src/main/java/com/lyn/pcode/models/food/FoodRepository.java
+++ src/main/java/com/lyn/pcode/models/food/FoodRepository.java
@@ -1,7 +1,14 @@
 package com.lyn.pcode.models.food;
 
+import com.lyn.pcode.web.dto.food.FoodsDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
     boolean existsByRestaurantIdAndName(Long restaurantId, String name);
+
+    // native query 를 이용해서 원하는 원하는 속성만 가져올 수도 있음
+    // List<FoodsDto> findIdAndNameAndPriceByRestaurantId(Long restaurantId);
+    List<Food> findAllByRestaurantId(Long restaurantId);
 }
