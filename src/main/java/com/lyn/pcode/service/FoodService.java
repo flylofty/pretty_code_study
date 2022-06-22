e6a0a86cd84b361307829983fc66de24e8dab5a6
Youngnam Lee <lyn00227@naver.com>
2022-06-22 20:00:36 +0900

    Revert "Merge pull request #8 from flylofty/dev/food"
    This reverts commit d24570acaaef812e67dee1800914050d1721b2b1, reversing
    changes made to 6ee68e8a0ba15a57cfc6e501182895bc18e88f6a.

--- src/main/java/com/lyn/pcode/service/FoodService.java
+++ src/main/java/com/lyn/pcode/service/FoodService.java
@@ -2,7 +2,6 @@ package com.lyn.pcode.service;
 
 import com.lyn.pcode.exception.FoodNameExistException;
 import com.lyn.pcode.models.food.FoodRepository;
-import com.lyn.pcode.web.dto.food.FoodsDto;
 import com.lyn.pcode.web.dto.food.SaveFoodDto;
 import com.lyn.pcode.web.dto.food.SaveFoodRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -39,11 +37,5 @@ public class FoodService {
 
     private boolean isFoodNameExist(Long restaurantId, String foodName) {
         return foodRepository.existsByRestaurantIdAndName(restaurantId, foodName);
-    }
-
-    public List<FoodsDto> getFoods(Long restaurantId) {
-        return foodRepository.findAllByRestaurantId(restaurantId).stream()
-                .map(FoodsDto::new)
-                .collect(Collectors.toList());
     }
 }
