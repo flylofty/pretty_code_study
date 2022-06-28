47cf8b51c5b049f7d1494ba6187479b2c907e79d
Youngnam Lee <lyn00227@naver.com>
2022-06-28 15:34:39 +0900

    feat: 엔티티 모델링

--- src/main/java/com/lyn/pcode/models/restaurant/Restaurant.java
+++ src/main/java/com/lyn/pcode/models/restaurant/Restaurant.java
@@ -1,29 +1,41 @@
 package com.lyn.pcode.models.restaurant;
 
 import com.lyn.pcode.models.Timestamped;
+import com.lyn.pcode.models.food.Food;
+import com.lyn.pcode.models.order.Order;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Restaurant extends Timestamped {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RESTAURANT_ID")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "RESTAURANT_NAME")
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "RESTAURANT_MIN_ORDER_PRICE")
     private Integer minOrderPrice;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "RESTAURANT_DELIVERY_FEE")
     private Integer deliveryFee;
+
+    @OneToMany(mappedBy = "restaurant", fetch = LAZY)
+    List<Order> Orders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "restaurant", fetch = LAZY)
+    List<Food> menu = new ArrayList<>();
 
     @Builder
     public Restaurant(String name, int minOrderPrice, int deliveryFee) {
