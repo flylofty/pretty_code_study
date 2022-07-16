17761ea627273e340f39eb0b613f1cca9b7bcb25
Youngnam Lee <lyn00227@naver.com>
2022-07-17 00:04:25 +0900

    test: 음식점 하나 조회 테스트

--- src/test/java/com/lyn/pcode/web/restaurant/RestaurantControllerV1Test.java
+++ src/test/java/com/lyn/pcode/web/restaurant/RestaurantControllerV1Test.java
@@ -11,12 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -62,4 +65,44 @@ class RestaurantControllerV1Test {
         assertEquals(5000, restaurant.getMinOrderPrice());
         assertEquals(2000, restaurant.getDeliveryFee());
     }
+
+    @Test
+    @DisplayName("GET /api/v1/restaurants 음식점 하나 조회")
+    void getRestaurantInfo() throws Exception {
+        // given
+        Restaurant restaurant = Restaurant.builder()
+                .name("쉐이크쉑 청담점")
+                .minOrderPrice(5000)
+                .deliveryFee(2000)
+                .build();
+
+        restaurantRepository.save(restaurant);
+
+        // expected
+        mockMvc.perform(get("/api/v1/restaurants")
+                        .contentType(APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].id").value(1L))
+                .andExpect(jsonPath("$.data[0].name").value("쉐이크쉑 청담점"))
+                .andExpect(jsonPath("$.data[0].minOrderPrice").value(5000))
+                .andExpect(jsonPath("$.data[0].deliveryFee").value(2000))
+                .andDo(print());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
