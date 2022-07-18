b052eb299a95782484a1e8e7d45255c1bcd5236e
Youngnam Lee <lyn00227@naver.com>
2022-07-18 22:57:15 +0900

    test: 메뉴 저장 테스트

--- src/test/java/com/lyn/pcode/web/restaurant/RestaurantControllerV1Test.java
+++ src/test/java/com/lyn/pcode/web/restaurant/RestaurantControllerV1Test.java
@@ -1,9 +1,11 @@
 package com.lyn.pcode.web.restaurant;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lyn.pcode.models.food.Food;
 import com.lyn.pcode.models.restaurant.Restaurant;
 import com.lyn.pcode.models.restaurant.RestaurantRepository;
-import com.lyn.pcode.service.RestaurantService;
+import com.lyn.pcode.web.dto.food.SaveFoodDto;
+import com.lyn.pcode.web.dto.food.SaveFoodRequestDto;
 import com.lyn.pcode.web.dto.restaurant.RestaurantSaveRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,9 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.*;
@@ -30,10 +35,13 @@ class RestaurantControllerV1Test {
     private MockMvc mockMvc;
 
     @Autowired
-    RestaurantService restaurantService;
+    EntityManager em;
 
     @Autowired
-    RestaurantRepository restaurantRepository;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
 
     @BeforeEach
     void clear() {
@@ -41,12 +49,12 @@ class RestaurantControllerV1Test {
     }
 
     @Test
-    @DisplayName("POST /api/v1/restaurants 요청 시 식당 기본 정보가 DB에 저장된다.")
+    @DisplayName("POST 요청 시 식당 기본 정보가 DB에 저장된다.")
     void saveRestaurant() throws Exception {
 
         //given
         RestaurantSaveRequestDto clientRequest = new RestaurantSaveRequestDto("쉐이크쉑 청담점", 5000, 2000);
-        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(clientRequest);
 
         //when
@@ -67,7 +75,7 @@ class RestaurantControllerV1Test {
     }
 
     @Test
-    @DisplayName("GET /api/v1/restaurants 음식점 하나 조회")
+    @DisplayName("GET 음식점 하나 조회")
     void getRestaurantInfo() throws Exception {
         // given
         Restaurant restaurant = Restaurant.builder()
@@ -88,6 +96,54 @@ class RestaurantControllerV1Test {
                 .andExpect(jsonPath("$.data[0].minOrderPrice").value(5000))
                 .andExpect(jsonPath("$.data[0].deliveryFee").value(2000))
                 .andDo(print());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("POST 특정 음식점 메뉴 등록")
+    void saveFoods() throws Exception {
+
+        // given
+        Restaurant restaurant = Restaurant.builder()
+                .name("쉐이크쉑 청담점")
+                .minOrderPrice(5000)
+                .deliveryFee(2000)
+                .build();
+
+        final Long restaurantId = 1L;
+
+        restaurantRepository.save(restaurant);
+
+        List<SaveFoodDto> foods = new ArrayList<>();
+        foods.add(new SaveFoodDto("쉑버거 더블", 10900));
+        foods.add(new SaveFoodDto("치즈 감자튀김", 4900));
+        foods.add(new SaveFoodDto("쉐이크", 5900));
+        SaveFoodRequestDto request = new SaveFoodRequestDto(foods);
+
+        String json = objectMapper.writeValueAsString(request);
+
+        // when
+        mockMvc.perform(post("/api/v1/restaurants/{restaurantId}/foods", restaurantId)
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        em.flush();
+        em.clear();
+
+        // then
+        assertEquals(1L, restaurantRepository.count());
+        Restaurant savedRestaurant = restaurantRepository.findAll().get(0);
+        List<Food> menu = savedRestaurant.getMenu();
+
+        int len = menu.size();
+        assertEquals(len, foods.size());
+        for (int i = 0; i < len; ++i) {
+            Food food = menu.get(i);
+            assertEquals(food.getName(), foods.get(i).getName());
+            assertEquals(Integer.parseInt(food.getPrice()), foods.get(i).getPrice());
+        }
     }
 }
 
